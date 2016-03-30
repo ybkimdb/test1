810b40247f9e700ad71c94fa6a45401020345388
yb kim <ybkimdb@gmail.com>
2016-03-30 11:02:24 +0900

    master-banch commint

--- con1/con1/Program.cs
+++ con1/con1/Program.cs
@@ -10,7 +10,6 @@ namespace con1
     {
         static void Main(string[] args)
         {
-            // comment intert  2nd commit
             // create branch  3dn commit
             // comit 3rd 
             Console.WriteLine("Hello World!");
